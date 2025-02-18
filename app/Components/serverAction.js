114d56d8e57a4551c56c34c13fa569e3f804e009
Behnam <adabzadehbehnam@gmail.com>
2025-02-18 10:26:21 +0330

    add responsive

--- app/Components/serverAction.js
+++ app/Components/serverAction.js
@@ -1,6 +1,8 @@
 
 export const fetchData = async()=>{
-    const data = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+    const data = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",{
+        cache : "no-store"
+    })
     const result = await data.json()
     return result
 
